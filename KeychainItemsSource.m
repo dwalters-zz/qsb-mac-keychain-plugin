535ee92cf231ad43f216b8207fa47982ade93649
Dan Walters <dan@danwalters.net>
2009-05-20 10:39:20 +0900

    Convert tabs to spaces.

--- KeychainItemsSource.m
+++ KeychainItemsSource.m
@@ -25,10 +25,10 @@ static OSStatus KeychainModified(SecKeychainEvent keychainEvent,
                                  SecKeychainCallbackInfo *info,
                                  void *context)
 {
-	HGSLogDebug(@"keychain modification event received");
-	KeychainItemsSource *source = (KeychainItemsSource *)context;
-	[source updateIndex];
-	return noErr;
+  HGSLogDebug(@"keychain modification event received");
+  KeychainItemsSource *source = (KeychainItemsSource *)context;
+  [source updateIndex];
+  return noErr;
 }
 
 @implementation KeychainItemsSource
@@ -37,12 +37,12 @@ static OSStatus KeychainModified(SecKeychainEvent keychainEvent,
   CFRetain(itemRef); // KeychainItem releases but doesn't retain
   KeychainItem *item = [[[KeychainItem alloc] initWithRef:itemRef] autorelease];
 
-	// create and return the result
+  // create and return the result
   NSString *name = [item label];
   NSString *urlString = [NSString stringWithFormat:@"keychain://%@/%@",
                          @"default",
                          [name stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-	NSMutableDictionary *attributes =
+  NSMutableDictionary *attributes =
     [NSMutableDictionary dictionaryWithObjectsAndKeys:
       keychainIcon_, kHGSObjectAttributeIconKey,
       item, kKeychainItemKey,
@@ -51,7 +51,7 @@ static OSStatus KeychainModified(SecKeychainEvent keychainEvent,
   if (action) {
     [attributes setObject:action forKey:kHGSObjectAttributeDefaultActionKey];
   }
-	return [HGSResult resultWithURL:[NSURL URLWithString:urlString]
+  return [HGSResult resultWithURL:[NSURL URLWithString:urlString]
                              name:name
                              type:kKeychainItemType
                            source:self
@@ -59,72 +59,72 @@ static OSStatus KeychainModified(SecKeychainEvent keychainEvent,
 }
 
 - (id)initWithConfiguration:(NSDictionary *)configuration {
-	if ((self = [super initWithConfiguration:configuration])) {
+  if ((self = [super initWithConfiguration:configuration])) {
     // use the keychain access icon
     NSWorkspace *ws = [NSWorkspace sharedWorkspace];
     keychainIcon_ = [ws iconForFile:[ws absolutePathForAppBundleWithIdentifier:@"com.apple.keychainaccess"]];
     [keychainIcon_ retain];
 
-		// build the initial index
-		[self updateIndex];
+    // build the initial index
+    [self updateIndex];
     
-		// register a callback for when the keychain is modified
-		OSStatus result = SecKeychainAddCallback(KeychainModified,
+    // register a callback for when the keychain is modified
+    OSStatus result = SecKeychainAddCallback(KeychainModified,
                                              kSecAddEventMask
                                              | kSecDeleteEventMask 
                                              | kSecUpdateEventMask 
                                              | kSecDefaultChangedEventMask 
                                              | kSecKeychainListChangedMask,
                                              self);
-		if (result != noErr) {
-			HGSLog(@"KeychainItemsSource: error %d while adding modification callback", result);
-		}
-	}
-	return self;
+    if (result != noErr) {
+      HGSLog(@"KeychainItemsSource: error %d while adding modification callback", result);
+    }
+  }
+  return self;
 }
 
 - (void)dealloc {
-	// remove the keychain modification callback
-	OSStatus result = SecKeychainRemoveCallback(KeychainModified);
-	if (result != noErr) {
-		HGSLog(@"KeychainItemsSource: error %d while removing modification callback", result);
-	}
+  // remove the keychain modification callback
+  OSStatus result = SecKeychainRemoveCallback(KeychainModified);
+  if (result != noErr) {
+    HGSLog(@"KeychainItemsSource: error %d while removing modification callback", result);
+  }
 
   [keychainIcon_ release];
-	[super dealloc];
+  [super dealloc];
 }
 
 - (void)searchSecurityClass:(SecItemClass)targetClass {
-	SecKeychainSearchRef searchRef;
-	OSStatus result = SecKeychainSearchCreateFromAttributes(NULL, targetClass, NULL, &searchRef);
-	if (result != noErr) {
-		HGSLog(@"KeychainItemsSource: error %d while starting search", result);
-		return;
-	}
-	
-	SecKeychainItemRef itemRef;
-	while ((result = SecKeychainSearchCopyNext(searchRef, &itemRef)) == noErr) {
-		
-		// create an indexable result for the item and index it
-		HGSResult* newResult = [self resultForItem:itemRef ofClass:targetClass];
-		if (newResult) {
-			[self indexResult:newResult];
-		}
+  SecKeychainSearchRef searchRef;
+  OSStatus result = SecKeychainSearchCreateFromAttributes(NULL, targetClass, NULL, &searchRef);
+  if (result != noErr) {
+    HGSLog(@"KeychainItemsSource: error %d while starting search", result);
+    return;
+  }
+  
+  SecKeychainItemRef itemRef;
+  while ((result = SecKeychainSearchCopyNext(searchRef, &itemRef)) == noErr) {
+    
+    // create an indexable result for the item and index it
+    HGSResult* newResult = [self resultForItem:itemRef ofClass:targetClass];
+    if (newResult) {
+      [self indexResult:newResult];
+    }
 
-		CFRelease(itemRef);
-	}
-	if (result != errSecItemNotFound) {
-		HGSLog(@"KeychainItemsSource: error %d while iterating through search results", result);
-	}
-	
-	CFRelease(searchRef);
+    CFRelease(itemRef);
+  }
+  if (result != errSecItemNotFound) {
+    HGSLog(@"KeychainItemsSource: error %d while iterating through search results", result);
+  }
+  
+  CFRelease(searchRef);
 }
 
 - (void)updateIndex {
-	HGSLogDebug(@"updating index");
-	[self clearResultIndex];
-	[self searchSecurityClass:kSecInternetPasswordItemClass];
-	[self searchSecurityClass:kSecGenericPasswordItemClass];
+  HGSLogDebug(@"updating index");
+  [self clearResultIndex];
+  [self searchSecurityClass:kSecInternetPasswordItemClass];
+  [self searchSecurityClass:kSecGenericPasswordItemClass];
 }
 
 @end
